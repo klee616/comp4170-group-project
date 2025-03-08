d5eda44f27d0cf313ebb18eb314935ab22806a00
klee616 <145302582+klee616@users.noreply.github.com>
2025-03-08 00:14:45 -0800

    update

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 
-
+//Index, list all pokemon
 app.get('/', pokemon.homeView)
+//Filter pokemon
 app.get('/filter', pokemon.filterPokemon)
+//Modify Pokemon, do not support alter pokemon type (has bugs, will fix later)
 app.get('/modify', pokemon.modifyPokemon)
+//View Pokemon
 app.get('/pokemon', pokemon.pokemon)
-app.get('/game', pokemon.pokemonGame)
+//Updata pokemon, and it will redirect to /pokemon page
 app.post('/updatePokemon', pokemon.updatePokemon)
+// Min Game, only support generation filter.
+app.get('/game', pokemon.pokemonGame)
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
