d567722ea7b70c273d82bbbf49f22cc71c42db95
klee616 <145302582+klee616@users.noreply.github.com>
2025-03-22 12:45:51 -0700

    delete images sets

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 //Index, list all pokemon
 app.get('/', pokemon.homeView)
