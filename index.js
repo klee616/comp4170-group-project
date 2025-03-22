618b30f9aa5e59a72b5a16f45289038f1a2638e3
klee616 <145302582+klee616@users.noreply.github.com>
2025-03-22 12:31:18 -0700

    fixed vecel problem

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ const pokemon = pokemonController;
 
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Index, list all pokemon
 app.get('/', pokemon.homeView)
